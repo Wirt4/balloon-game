b3eece5e7ad4504b6ad89b91cdb2ee5546740a82
Wirt4 <wirt.salthouse@gmail.com>
2021-04-08 20:21:18 -0700

    image finally loads in browser

--- SpriteDrawing.js
+++ SpriteDrawing.js
@@ -1,2 +1,11 @@
-"use strict";
-alert("script called");
+//below design pattern seems to work
+//note: the 5 year old answer code does NOT... so this is a tricky workthrough
+var img = new Image();
+// declare a function to call once the image has loaded
+img.onload = function(){
+  var canvas = document.getElementById('myCanvas');
+  var context = canvas.getContext('2d');
+  context.drawImage(img, 0,0);
+}
+// now set the image's src
+img.src = "spr_balloon.png";
