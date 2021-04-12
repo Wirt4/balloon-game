e78712f0a9acd8c0b2380fbaf5220b28ed025d99
Wirt4 <wirt.salthouse@gmail.com>
2021-04-11 17:36:02 -0700

    balloon centered on cursor

--- js/bloonGame.js
+++ js/bloonGame.js
@@ -6,7 +6,8 @@ var Game = {
     backgroundSprite: undefined,
     balloonSprite: undefined,
     balloonPosition: { x: 0, y: 50 },
-    backgroundMusic: undefined
+    backgroundMusic: undefined,
+    balloonOrigin: { x: 0, y: 50 }
 };
 
 function handleMouseMove(evt) {
@@ -34,10 +35,10 @@ Game.clearCanvas = function () {
     Game.canvasContext.clearRect(0, 0, Game.canvas.width, Game.canvas.height);
 };
 
-Game.drawImage = function (sprite, position) {
+Game.drawImage = function (sprite, position, origin) {
     Game.canvasContext.save();
     Game.canvasContext.translate(position.x, position.y);
-    Game.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height, 0, 0, sprite.width, sprite.height);
+    Game.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height, -origin.x, -origin.y, sprite.width, sprite.height);
     Game.canvasContext.restore();
 };
 
@@ -51,11 +52,12 @@ Game.mainLoop = function () {
 Game.update = function () {
     document.onmousemove = handleMouseMove;
     var d = new Date();
-   // Game.balloonPosition.x = d.getTime() % Game.canvas.width;
+    // Game.balloonPosition.x = d.getTime() % Game.canvas.width;
 };
 
 Game.draw = function () {
-    Game.drawImage(Game.backgroundSprite, { x: 0, y: 0 });
-    Game.drawImage(Game.balloonSprite, Game.balloonPosition);
-    Game.drawImage(Game.balloonSprite, { x: 50, y: 100 });
+    Game.drawImage(Game.backgroundSprite, { x: 0, y: 0 }, { x: 0, y: 0 });
+    Game.balloonOrigin = { x: Game.balloonSprite.width / 2, y: Game.balloonSprite.height / 2 };
+    Game.drawImage(Game.balloonSprite, Game.balloonPosition,Game.balloonOrigin);
+    // Game.drawImage(Game.balloonSprite, { x: 50, y: 100 }, { x: 0, y: 0 });
 };
