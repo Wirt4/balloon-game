4f93b4b389d0a23e8264342ade425ac1c91be5cf
Wirt4 <wirt.salthouse@gmail.com>
2021-04-19 14:53:16 -0700

    cannon moves

--- js/bloonGame.js
+++ js/bloonGame.js
@@ -4,17 +4,16 @@ function handleMouseMove(evt) {
     Game.mousePosition = { x: evt.pageX, y: evt.pageY };
 }
 
-var Mouse = {
-    position: { x: 0, y: 0 }
-};
-
 var Game = {
-    balloonOrigin: { x: 0, y: 0 },
     canvas: undefined,
     canvasContext: undefined,
     backgroundSprite: undefined,
-    balloonSprite: undefined,
-    mousePosition: { x: 0, y: 0 }
+    mousePosition: { x: 0, y: 0 },
+    cannonBarrelSprite: undefined,
+    //below are constant values
+    cannonPosition: { x: 72, y: 400 },
+    cannonOrigin: { x: 34, y: 34 },
+    cannonRotation: 0
 };
 
 Game.clearCanvas = function () {
@@ -22,26 +21,30 @@ Game.clearCanvas = function () {
 }
 
 Game.update = function () {
-    //stub per walk-through notes
+    var opp = Game.mousePosition.y - Game.cannonPosition.y;
+    var adj = Game.mousePosition.x - Game.cannonPosition.x;
+    Game.cannonRotation = Math.atan2(opp, adj);
 }
 
-Game.drawImage = function (image, position, origin) {
+Game.drawImage = function (image, position, rotation, origin) {
     //note the save, restore pattern
     Game.canvasContext.save();
     //note the first translate, then draw idiom
     Game.canvasContext.translate(position.x, position.y);
+    Game.canvasContext.rotate(rotation);
     Game.canvasContext.drawImage(image, 0, 0, image.width, image.height, -origin.x, -origin.y, image.width, image.height);
     Game.canvasContext.restore();
 }
 
 Game.draw = function () {
-    Game.drawImage(Game.backgroundSprite, { x: 0, y: 0 }, { x: 0, y: 0 });
-    Game.balloonOrigin = { x: Game.balloonSprite.width / 2, y: Game.balloonSprite.height / 2 };
-    Game.drawImage(Game.balloonSprite, Game.mousePosition, Game.balloonOrigin);
+    Game.clearCanvas();
+    Game.drawImage(Game.backgroundSprite, { x: 0, y: 0 }, 0, { x: 0, y: 0 });
+    //Game.balloonOrigin = { x: Game.balloonSprite.width / 2, y: Game.balloonSprite.height / 2 };
+    Game.drawImage(Game.cannonBarrelSprite, Game.cannonPosition, Game.cannonRotation, Game.cannonOrigin);
 }
 
 Game.mainLoop = function () {
-    Game.clearCanvas(); //implement
+    // Game.clearCanvas(); //implement
     Game.update();
     Game.draw();
     window.setTimeout(Game.mainLoop, 1000 / 60);
@@ -54,9 +57,13 @@ Game.start = function () {
     document.onmousemove = handleMouseMove;
     Game.backgroundSprite = new Image();
     Game.backgroundSprite.src = "assets/images/spr_background.jpg";
-    Game.balloonSprite = new Image();
-    Game.balloonSprite.src = "assets/images/spr_balloon.png";
+    //Game.balloonSprite = new Image();
+    //Game.balloonSprite.src = "assets/images/spr_balloon.png";
+    Game.cannonBarrelSprite = new Image();
+    Game.cannonBarrelSprite.src = "assets/images/spr_cannon_barrel.png";
     window.setTimeout(Game.mainLoop, 500);
 };
 
 document.addEventListener('DOMContentLoaded', Game.start);
+
+
